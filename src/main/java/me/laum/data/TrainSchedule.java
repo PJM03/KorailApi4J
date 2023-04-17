0b1f510ce4b984fe1666f67dae3cc0f7332a0a96
박정민 <46414840+PJM03@users.noreply.github.com>
2023-04-17 06:46:59 +0000

    Pending changes exported from your codespace

--- src/main/java/me/laum/data/TrainSchedule.java
+++ src/main/java/me/laum/data/TrainSchedule.java
@@ -1,6 +1,18 @@
 package me.laum.data;
 
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -13,14 +25,34 @@ public class TrainSchedule {
     private final int cost;
     @SerializedName("arrplacename")
     private final String arrivalName;
+
     @SerializedName("arrplandtime")
-    private final long arrivalTime;
+    @JsonAdapter(DateAdapter.class)
+    private final LocalDateTime arrivalTime;
+
     @SerializedName("depplacename")
     private final String departureName;
+    
     @SerializedName("depplandtime")
-    private final long departureTime;
+    @JsonAdapter(DateAdapter.class)
+    private final LocalDateTime departureTime;
+    
     @SerializedName("traingradename")
     private final String trainName;
     @SerializedName("trainno")
     private final int trainNumber;
+
+    private static class DateAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+        private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        @Override
+        public LocalDateTime deserialize(JsonElement json, Type t, JsonDeserializationContext ctx) throws JsonParseException {
+            return LocalDateTime.parse(json.getAsString(), FMT);
+        }
+
+        @Override
+        public JsonElement serialize(LocalDateTime src, Type t, JsonSerializationContext ctx) {
+            return ctx.serialize(src.format(FMT));
+        }
+    }
 }
+
